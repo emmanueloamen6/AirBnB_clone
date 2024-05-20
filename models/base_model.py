4af1a1ae3d2e6afe0ff2a75bb367c68f6550e9b4
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-05-20 10:52:03 -0700

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -6,34 +6,30 @@ from datetime import datetime
 
 
 class BaseModel:
+    """Inistialize the class BaseModel. """
     def __init__(self):
+        """ Constructor. """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
     def save(self):
-        """
-
-        """
+        """ Save the instance. """
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        """
-
-        """
+        """ Dictionary of the instance. """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
-
         return inst_dict
 
     def __str__(self):
-        """
-
-        """
+        """ String of the instance. """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
 
 if __name__ == "__main__":
     my_model = BaseModel()
