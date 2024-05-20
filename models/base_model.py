207dd616e72859eaf2ef331d760ac2c38b83aef7
emmanueeloamen6 <emmanueloamen6@gmail.com>
2024-05-20 12:13:02 -0700

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,26 @@ from datetime import datetime
 
 class BaseModel:
     """Inistialize the class BaseModel. """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Constructor. """
+        time_f = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_f))
+                else:
+                    setattr(self, key, value)
+        models.storage.new(self)
 
     def save(self):
         """ Save the instance. """
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """ Dictionary of the instance. """
@@ -43,3 +54,11 @@ if __name__ == "__main__":
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
